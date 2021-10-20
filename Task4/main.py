dbbfa6bbf56fdc32fcad85de9f8e116d034c9580
Kamil Kowalewski <szamil24@gmail.com>
2021-10-20 08:44:22 +0200

    Add utils.py

--- Task4/main.py
+++ Task4/main.py
@@ -1,6 +1,7 @@
-import subprocess
-import sys
 from argparse import ArgumentParser, Namespace
+
+from module.utils import check_if_exists_in_args, check_types_check_style, compile_to_pyc, \
+    display_finish
 
 """
     How to run:
@@ -22,26 +23,6 @@ def prepare_args() -> Namespace:
     arg_parser = ArgumentParser()
 
     return arg_parser.parse_args()
-
-
-# UTIL ----------------------------------------------------------------------- #
-def check_types_check_style() -> None:
-    subprocess.call(["mypy", "."])
-    subprocess.call(["flake8", "."])
-
-
-def compile_to_pyc() -> None:
-    subprocess.call(["python", "-m", "compileall", "."])
-
-
-def check_if_exists_in_args(arg: str) -> bool:
-    return arg in sys.argv
-
-
-def display_finish() -> None:
-    print("------------------------------------------------------------------------")
-    print("FINISHED")
-    print("------------------------------------------------------------------------")
 
 
 # __MAIN__ ------------------------------------------------------------------- #
