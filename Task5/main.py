f9c4cf811e9dcc4cc1df2d18298ced46165b1acc
Kamil Kowalewski <szamil24@gmail.com>
2021-10-30 18:49:39 +0200

    Add basic structure of project

--- Task5/main.py
+++ Task5/main.py
@@ -1,19 +1,43 @@
 from argparse import ArgumentParser, Namespace
+from typing import Dict, Tuple
 
-from module.utils import check_if_exists_in_args, check_types_check_style, compile_to_pyc, \
-    display_finish
+import pandas as pd
+
+from module.reader import read_dataset_1, read_dataset_2, read_dataset_3
+from module.utils import create_directory, display_finish, run_main
 
 """
-    How to run:
+    How to run: python main.py -s -a 
         
 """
 
-
 # VAR ------------------------------------------------------------------------ #
+RESULTS_DIR = "results/"
+
+ALGORITHM_SETUP: Dict[str, Tuple] = {
+
+}
+
 
 # MAIN ----------------------------------------------------------------------- #
 def main() -> None:
     args = prepare_args()
+    chosen_algorithm_name = args.algorithm
+    save_results = args.save
+    create_directory(RESULTS_DIR)
+
+    print("Reading datasets ...")
+    datasets: Dict[str, pd.DataFrame] = {
+        "dataset_1": read_dataset_1(),
+        "dataset_2": read_dataset_2(),
+        "dataset_3": read_dataset_3(),
+    }
+
+    for dataset in datasets:
+        print(f"Detecting dataset: {dataset} ...")
+
+        if save_results:
+            pass
 
     display_finish()
 
@@ -22,14 +46,16 @@ def main() -> None:
 def prepare_args() -> Namespace:
     arg_parser = ArgumentParser()
 
+    arg_parser.add_argument(
+        "-a", "--algorithm", type=str, choices=ALGORITHM_SETUP.keys(), help="Name of algorithm"
+    )
+    arg_parser.add_argument(
+        "-s", "--save", default=False, action="store_true", help="Save results to files"
+    )
+
     return arg_parser.parse_args()
 
 
 # __MAIN__ ------------------------------------------------------------------- #
 if __name__ == "__main__":
-    if check_if_exists_in_args("-t"):
-        check_types_check_style()
-    elif check_if_exists_in_args("-b"):
-        compile_to_pyc()
-    else:
-        main()
+    run_main(main)
