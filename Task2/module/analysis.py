66f79e5850d6a3a2b176153334a7e6ce57c1de2f
Kamil Kowalewski <szamil24@gmail.com>
2021-11-09 17:16:52 +0100

    Changes in analysis.py

--- Task2/module/analysis.py
+++ Task2/module/analysis.py
@@ -1,8 +1,18 @@
 import pandas as pd
+from sklearn.cluster import AgglomerativeClustering, DBSCAN, KMeans
+from sklearn.neighbors import LocalOutlierFactor
 
 
 def clusterize(dataset: pd.DataFrame, clusterizer) -> float:
-    clusterizer.fit_predict(dataset)
-    X_scores = clusterizer.negative_outlier_factor_
-    radius = (X_scores.max() - X_scores) / (X_scores.max() - X_scores.min())
-    return radius
+    y_pred = clusterizer.fit_predict(dataset)
+
+    if isinstance(clusterizer, KMeans):
+        return -1.0
+    if isinstance(clusterizer, AgglomerativeClustering):
+        return -1.0
+    if isinstance(clusterizer, DBSCAN):
+        return -1.0
+    if isinstance(clusterizer, LocalOutlierFactor):
+        X_scores = clusterizer.negative_outlier_factor_
+        # Calculate radius
+        return (X_scores.max() - X_scores) / (X_scores.max() - X_scores.min())
