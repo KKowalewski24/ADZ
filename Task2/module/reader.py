c92c9dc9120828d4beaae422e25d1b1c595b243e
Kamil Kowalewski <szamil24@gmail.com>
2021-11-09 14:19:51 +0100

    Fixes

--- Task2/module/reader.py
+++ Task2/module/reader.py
@@ -1,8 +1,11 @@
+import os
+
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 DATASET_DIR: str = "data/"
+SYNTHETIC_DATASET_PATH = f"{DATASET_DIR}synthetic_dataset.csv"
 
 
 # https://www.kaggle.com/parulpandey/palmer-archipelago-antarctica-penguin-data?select=penguins_size.csv
@@ -22,12 +25,14 @@ def read_dataset_3() -> pd.DataFrame:
 
 
 def read_synthetic_dataset() -> pd.DataFrame:
-    np.random.seed(42)
-    X_inliers = 0.3 * np.random.randn(200, 2)
-    X_inliers = np.r_[X_inliers + 2, X_inliers - 2]
-    X_outliers = np.random.uniform(low=-4, high=4, size=(20, 2))
+    if not os.path.exists(SYNTHETIC_DATASET_PATH):
+        np.random.seed(42)
+        X_inliers = 0.3 * np.random.randn(200, 2)
+        X_inliers = np.r_[X_inliers + 2, X_inliers - 2]
+        X_outliers = np.random.uniform(low=-4, high=4, size=(20, 2))
+        pd.DataFrame(np.r_[X_inliers, X_outliers]).to_csv(SYNTHETIC_DATASET_PATH, index=False)
 
-    return pd.DataFrame(np.r_[X_inliers, X_outliers])
+    return pd.read_csv(SYNTHETIC_DATASET_PATH)
 
 
 def read_iris_ds() -> pd.DataFrame:
