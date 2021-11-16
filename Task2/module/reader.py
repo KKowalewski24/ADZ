0d4c84ebd78b7012c63a1bd5fb33a544a3b7eb49
Kamil Kowalewski <szamil24@gmail.com>
2021-11-16 22:07:56 +0100

    Add utils function in reader.py

--- Task2/module/reader.py
+++ Task2/module/reader.py
@@ -1,7 +1,10 @@
 import os
+from typing import List
 
 import numpy as np
 import pandas as pd
+from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
 
 DATASET_DIR: str = "data/"
 SYNTHETIC_DATASET_PATH = f"{DATASET_DIR}synthetic_dataset.csv"
@@ -16,3 +19,15 @@ def read_synthetic_dataset() -> pd.DataFrame:
         pd.DataFrame(np.r_[X_inliers, X_outliers]).to_csv(SYNTHETIC_DATASET_PATH, index=False)
 
     return pd.read_csv(SYNTHETIC_DATASET_PATH)
+
+
+def _encode_labels(df: pd.DataFrame, column_names: List[str]) -> pd.DataFrame:
+    label_encoder = LabelEncoder()
+    for column_name in column_names:
+        df[column_name] = label_encoder.fit_transform(df[column_name])
+
+    return df
+
+
+def _normalize_data(df: pd.DataFrame) -> pd.DataFrame:
+    return preprocessing.normalize(df, axis=0)
