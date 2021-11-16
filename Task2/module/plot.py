83477ce63b41df3372e86056a655ad1691776b59
Kamil Kowalewski <szamil24@gmail.com>
2021-11-16 22:46:53 +0100

    Add PCA

--- Task2/module/plot.py
+++ Task2/module/plot.py
@@ -2,6 +2,7 @@ from typing import Dict
 
 import pandas as pd
 from matplotlib import pyplot as plt
+from sklearn.decomposition import PCA
 
 from module.utils import prepare_filename
 
@@ -18,9 +19,16 @@ colors: Dict[int, str] = {
 
 
 def draw_plots(df: pd.DataFrame, name: str, results_dir: str, save_data: bool) -> None:
+    reduced_data = PCA(n_components=2).fit_transform(df)
     plt.scatter(
-        df.iloc[:, 0], df.iloc[:, 1], color=df.cluster.map(colors), s=15.0
+        reduced_data[:, 0], reduced_data[:, 1], color=df.cluster.map(colors), s=15.0
     )
+
+    # TODO Check if PCA works as expected
+    # plt.scatter(
+    #     df.iloc[:, 0], df.iloc[:, 1], color=df.cluster.map(colors), s=15.0
+    # )
+
     _set_descriptions(name, "", "")
     _show_and_save(name, results_dir, save_data)
 
