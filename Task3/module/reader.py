ea98775bc9d5428b13383972c02833265d7daf7a
Kamil Kowalewski <szamil24@gmail.com>
2021-11-29 17:59:03 +0100

    Add outliers

--- Task3/module/reader.py
+++ Task3/module/reader.py
@@ -9,12 +9,9 @@ DATASET_DIR: str = "data/"
 
 # https://www.kaggle.com/rakannimer/air-passengers
 def read_air_passengers() -> Tuple[pd.DataFrame, np.ndarray]:
-    # 10% of outliers
-    indexes = [4, 6, 7, 39, 40, 50, 52, 79, 91, 92, 105, 110, 117, 136, 137]
-    values = [
-        797, 84220, 59598, 46, 30620, 63580, 154215, 70546, 50,
-        88987, 67738, 8029, 13743, 460130, 95508
-    ]
+    # 11% of outliers
+    indexes = [6, 33, 36, 51, 60, 121, 122, 123, 124, 135, 136, 137, 138, 139, 140, 141]
+    values = [205, 600, 150, 315, 150, 340, 340, 340, 340, 620, 620, 620, 620, 620, 620, 620]
 
     return _add_outliers_set_datetime(
         pd.read_csv(f"{DATASET_DIR}air_passengers.csv"), indexes, values, "date", "passengers"
@@ -23,16 +20,13 @@ def read_air_passengers() -> Tuple[pd.DataFrame, np.ndarray]:
 
 # https://www.kaggle.com/bulentsiyah/for-simple-exercises-time-series-forecasting?select=Alcohol_Sales.csv
 def read_alcohol_sales() -> Tuple[pd.DataFrame, np.ndarray]:
-    # 10% of outliers
+    # 6% of outliers
     indexes = [
-        7, 32, 36, 42, 60, 61, 71, 82, 87, 103, 105, 105, 108, 111, 115, 129, 137, 141, 150, 180,
-        183, 185, 187, 204, 215, 218, 230, 235, 245, 268, 268, 322
+        72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 128, 151, 315, 316, 317, 318, 319, 320
     ]
-
     values = [
-        23156, 19940, 14759, 19987, 19269, 22246, 17283, 20015, 18974, 15473, 16370, 18360, 15439,
-        14853, 24657, 24940, 1263, 331, 322, 1669, 1059, 1625, 1432, 2440, 2324, 1650, 1242, 408, 830,
-        315, 2068, 303,
+        5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 2000, 8300, 9000,
+        9000, 9000, 9000, 9000, 9000
     ]
 
     return _add_outliers_set_datetime(
@@ -46,17 +40,12 @@ def read_gold_price() -> Tuple[pd.DataFrame, np.ndarray]:
     path_preprocessing = _add_suffix(path)
     _gold_price_preprocessing(path, path_preprocessing)
 
-    # 10% of outliers
+    # 4% of outliers
     indexes = [
-        5, 14, 21, 26, 29, 31, 34, 51, 58, 71, 73, 94, 153, 156, 172, 182, 198, 209, 209, 211, 212,
-        228, 233, 233, 238, 253, 267, 267, 268, 275, 280, 284, 291, 291, 298, 300, 314, 319, 355, 370,
-        388, 398, 398, 416, 416, 439, 446, 464, 468, 477, 477, 488, 514
+        37, 38, 140, 141, 142, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 306, 441
     ]
-
     values = [
-        2228, 4500, 4552, 3386, 2730, 3011, 2881, 3701, 3705, 2272, 2632, 1303, 1660, 1628, 3236,
-        2800, 1872, 2483, 1741, 3914, 2463, 4273, 4802, 4187, 1480, 4497, 2951, 4158, 3089, 2442, 21,
-        23, 3, 8, 14, 23, 28, 15, 3, 13, 18, 26, 26, 15, 4, 3, 30, 9, 5, 25, 24, 27, 9,
+        800, 800, 250, 250, 250, 400, 400, 400, 400, 400, 400, 400, 400, 400, 400, 400, 400, 320, 120
     ]
 
     return _add_outliers_set_datetime(
