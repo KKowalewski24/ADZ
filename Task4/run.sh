ae5f088c1eef9fc3a6a9aa38184ca92091f6275e
Kamil Kowalewski <szamil24@gmail.com>
2021-12-22 10:34:51 +0100

    Remove old code + move reader to main

--- Task4/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python main.py -s -ds http -d fast_abod
-python main.py -s -ds mammography -d fast_abod
-python main.py -s -ds synthetic -d fast_abod
-
-#python main.py -s -ds http -d bnl
-#python main.py -s -ds mammography -d bnl
-#python main.py -s -ds synthetic -d bnl
--- Task4/simplified_code/main.py
+++ Task4/simplified_code/main.py
@@ -1,10 +1,56 @@
-import numpy as np
+from typing import Tuple
+
+import h5py
 import matplotlib.pyplot as plt
-from sklearn.metrics import precision_recall_curve
+import numpy as np
 from pyod.models.abod import ABOD
-from pyod.models.lof import LOF
 from pyod.models.cof import COF
-from reader import read_http_dataset, read_mammography_dataset, read_synthetic_dataset
+from pyod.models.lof import LOF
+from pyod.utils.data import generate_data
+from scipy.io import loadmat
+from sklearn.metrics import precision_recall_curve
+
+DATASET_DIR: str = "../data/"
+RANDOM_STATE_VALUE = 21
+
+
+def read_synthetic_dataset() -> Tuple[np.ndarray, np.ndarray]:
+    return generate_data(
+        n_train=200,
+        n_features=2,
+        contamination=0.1,
+        train_only=True,
+        random_state=RANDOM_STATE_VALUE,
+        behaviour="new",
+    )
+
+
+def read_mammography_dataset() -> Tuple[np.ndarray, np.ndarray]:
+    file = loadmat(f"{DATASET_DIR}mammography.mat")
+    X = file["X"]
+    y = file["y"].squeeze().astype(np.int32)
+    return X, y
+
+
+def read_http_dataset() -> Tuple[np.ndarray, np.ndarray]:
+    file = h5py.File(f"{DATASET_DIR}http.mat")
+    X = np.array(file["X"]).transpose()
+    y = np.array(file["y"]).transpose().squeeze().astype(np.int32)
+
+    np.random.seed(47)
+    X_normal = _random_samples(X[y == 0], 0.01)
+    X_outliers = _random_samples(X[y == 1], 0.02)
+
+    X = np.concatenate([X_normal, X_outliers])
+    y = np.concatenate(
+        [np.zeros((len(X_normal),)), np.zeros((len(X_outliers),)) + 1]
+    ).astype(np.int32)
+
+    return X, y
+
+
+def _random_samples(X, fraction):
+    return X[np.random.randint(len(X), size=(int(fraction * len(X)),))]
 
 
 def find_outliers(dataset, detector, **params):
